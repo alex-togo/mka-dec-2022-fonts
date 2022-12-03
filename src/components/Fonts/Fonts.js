a47b0a5b71bcd062348d7db35fed76e0f74033dd
Alex Togo <helloalextogo@gmail.com>
2022-12-02 21:05:37 -0500

    add fonts, initial styling

---
+++ src/components/Fonts/Fonts.js
@@ -0,0 +1,33 @@
+import "./style.css";
+
+const Fonts = () => {
+  return (
+    <div className="container">
+      <h4>Hello MKA! This is an example of how to switch between fonts.</h4>
+      <p className="p1">
+        Hello MKA! This is an example of how to switch between fonts.
+      </p>
+      <p className="p2">
+        Hello MKA! This is an example of how to switch between fonts.
+      </p>
+      <p className="p3">
+        Hello MKA! This is an example of how to switch between fonts.
+      </p>
+      <p className="p4">
+        Hello MKA! This is an example of how to switch between fonts.
+      </p>
+      <br />
+      <p className="p5 variable">
+        Hello MKA! This is an example of a variable font.
+      </p>
+      <p className="p6 variable">
+        Hello MKA! This is an example of a variable font.
+      </p>
+      <p className="p7 variable">
+        Hello MKA! This is an example of a variable font.
+      </p>
+    </div>
+  );
+};
+
+export default Fonts;
