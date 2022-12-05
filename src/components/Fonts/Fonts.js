6232cb48c83f6a586cadf132c701b4403dc1b262
Alex Togo <helloalextogo@gmail.com>
2022-12-05 10:33:58 -0500

    feature: variable font slider

--- src/components/Fonts/Fonts.js
+++ src/components/Fonts/Fonts.js
@@ -1,6 +1,13 @@
 import "./style.css";
+import { useState, useRef } from "react";
 
 const Fonts = () => {
+  const [sliderVal, setSliderVal] = useState(100);
+  const sliderRef = useRef();
+
+  const handleSlider = (e) => {
+    setSliderVal(sliderRef.current.value);
+  };
   return (
     <div className="container">
       <h4>Hello MKA! This is an example of how to switch between fonts.</h4>
@@ -23,9 +30,21 @@ const Fonts = () => {
       <p className="p6 variable">
         Hello MKA! This is an example of a variable font.
       </p>
-      <p className="p7 variable">
+      <p className="p7 variable" style={{ fontWeight: sliderVal }}>
         Hello MKA! This is an example of a variable font.
       </p>
+      <input
+        ref={sliderRef}
+        type="range"
+        min="100"
+        max="1000"
+        value={sliderVal}
+        step="10"
+        name="Variable Font Weight"
+        onChange={handleSlider}
+        className="slider"
+      />
+      <label htmlFor="Variable Font Weight">Variable Font Weight</label>
     </div>
   );
 };
